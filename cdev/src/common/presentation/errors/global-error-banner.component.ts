8f899b801a72c2bd055206243da9dd7436823888
gritlabs1 <gritlabs1@gmail.com>
2026-05-07 19:08:04 -0400

    Materialize global error banner

--- cdev/src/common/presentation/errors/global-error-banner.component.ts
+++ cdev/src/common/presentation/errors/global-error-banner.component.ts
@@ -1,9 +1,12 @@
 import { Component, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { GlobalErrorState } from './global-error-state';
 
 @Component({
   selector: 'cdev-global-error-banner',
   standalone: true,
+  imports: [MatButtonModule, MatCardModule],
   templateUrl: './global-error-banner.component.html',
   styleUrl: './global-error-banner.component.css',
 })
